9a49b12d5262fe3a18a54e2aabab6aeb83b5fb60
Alkaid114514 <xinlai.gao2006@outlook.com>
2025-02-28 22:31:28 +0800

    ik初步编写

--- controllers/new_hexapod/Hexapod.cpp
+++ controllers/new_hexapod/Hexapod.cpp
@@ -57,3 +57,72 @@ float* Hexapod::ik(Vector3 vector3)
     float angles[3] = { theta1,theta2,theta3 };
     return angles;
 }
+
+void Hexapod::move(Vector3 velocity, float omega)
+{
+	Vector3 v = velocity;
+	v = v.normalize();
+	Vector3 w = Vector3(0.0f, 0.0f, omega);
+    Vector3 r = v.cross(w).normalize() * v.magnitude() / omega;
+
+	Vector3 v1 = v + w.cross(Vector3(0, 0, 1).normalize());
+	Vector3 v2 = v - w.cross(Vector3(0, 0, 1).normalize());
+	Vector3 v3 = v + w.cross(Vector3(0, 0, 1).normalize());
+	Vector3 v4 = v - w.cross(Vector3(0, 0, 1).normalize());
+	Vector3 v5 = v + w.cross(Vector3(0, 0, 1).normalize());
+	Vector3 v6 = v - w.cross(Vector3(0, 0, 1).normalize());
+	float* angles1 = ik(v1);
+	float* angles2 = ik(v2);
+	float* angles3 = ik(v3);
+	float* angles4 = ik(v4);
+	float* angles5 = ik(v5);
+	float* angles6 = ik(v6);
+	setPose(angles1, angles2, angles3, angles4, angles5, angles6);
+}
+
+void Hexapod::setTargets(Vector3 BRtarget, Vector3 MRtarget, Vector3 FRtarget, Vector3 BLtarget, Vector3 MLtarget, Vector3 FLtarget)
+{
+    Vector3 tmp = BRtarget - center2BackRightRoot;
+    Vector3 BRtargetBiased = Vector3(
+        cos(center2BackRightRootTheta) * tmp.x + sin(center2BackRightRootTheta) * tmp.y,
+		-sin(center2BackRightRootTheta) * tmp.x + cos(center2BackRightRootTheta) * tmp.y,
+		tmp.z
+        );
+	tmp = MRtarget - center2MiddleRightRoot;
+	Vector3 MRtargetBiased = Vector3(
+		cos(center2MiddleRightRootTheta) * tmp.x + sin(center2MiddleRightRootTheta) * tmp.y,
+		-sin(center2MiddleRightRootTheta) * tmp.x + cos(center2MiddleRightRootTheta) * tmp.y,
+		tmp.z
+	);
+	tmp = FRtarget - center2FrontRightRoot;
+	Vector3 FRtargetBiased = Vector3(
+		cos(center2FrontRightRootTheta) * tmp.x + sin(center2FrontRightRootTheta) * tmp.y,
+		-sin(center2FrontRightRootTheta) * tmp.x + cos(center2FrontRightRootTheta) * tmp.y,
+		tmp.z
+	);
+	tmp = BLtarget - center2BackLeftRoot;
+	Vector3 BLtargetBiased = Vector3(
+		cos(center2BackLeftRootTheta) * tmp.x + sin(center2BackLeftRootTheta) * tmp.y,
+		-sin(center2BackLeftRootTheta) * tmp.x + cos(center2BackLeftRootTheta) * tmp.y,
+		tmp.z
+	);
+	tmp = MLtarget - center2MiddleLeftRoot;
+	Vector3 MLtargetBiased = Vector3(
+		cos(center2MiddleLeftRootTheta) * tmp.x + sin(center2MiddleLeftRootTheta) * tmp.y,
+		-sin(center2MiddleLeftRootTheta) * tmp.x + cos(center2MiddleLeftRootTheta) * tmp.y,
+		tmp.z
+	);
+	tmp = FLtarget - center2FrontLeftRoot;
+	Vector3 FLtargetBiased = Vector3(
+		cos(center2FrontLeftRootTheta) * tmp.x + sin(center2FrontLeftRootTheta) * tmp.y,
+		-sin(center2FrontLeftRootTheta) * tmp.x + cos(center2FrontLeftRootTheta) * tmp.y,
+		tmp.z
+	);
+	float* angles1 = ik(BRtargetBiased);
+	float* angles2 = ik(MRtargetBiased);
+	float* angles3 = ik(FRtargetBiased);
+	float* angles4 = ik(BLtargetBiased);
+	float* angles5 = ik(MLtargetBiased);
+	float* angles6 = ik(FLtargetBiased);
+	setPose(angles1, angles2, angles3, angles4, angles5, angles6);
+}
--- controllers/new_hexapod/Hexapod.h
+++ controllers/new_hexapod/Hexapod.h
@@ -20,6 +20,20 @@ private:
 	Leg BackLeftLeg;
 	Leg MiddleLeftLeg;
 	Leg FrontLeftLeg;
+
+	const Vector3 center2BackRightRoot = Vector3(0.059, -0.083, 0);
+	const Vector3 center2MiddleRightRoot = Vector3(0.08, 0, 0);
+	const Vector3 center2FrontRightRoot = Vector3(0.064, 0.082, 0);
+	const Vector3 center2BackLeftRoot = Vector3(-0.059, -0.083, 0);
+	const Vector3 center2MiddleLeftRoot = Vector3(-0.08, 0, 0);
+	const Vector3 center2FrontLeftRoot = Vector3(-0.064, 0.082, 0);
+
+	const float center2BackRightRootTheta = -0.785395;
+	const float center2MiddleRightRootTheta = 0;
+	const float center2FrontRightRootTheta = 0.785398;
+	const float center2BackLeftRootTheta = -2.3562;
+	const float center2MiddleLeftRootTheta = 0;
+	const float center2FrontLeftRootTheta = 2.3562;
 public:
 	Hexapod();
 	~Hexapod();
@@ -27,5 +41,7 @@ public:
 	void setPose(float* backRightAngles, float* middleRightAngles, float* frontRightAngles,
 		float* backLeftAngles, float* middleLeftAngles, float* frontLeftAngles);
 	float* ik(Vector3 vector3);
+	void move(Vector3 velocity,float omega);
+	void setTargets(Vector3 BRtarget,Vector3 MRtarget,Vector3 FRtarget,Vector3 BLtarget,Vector3 MLtarget,Vector3 FLtarget);
 };
 
